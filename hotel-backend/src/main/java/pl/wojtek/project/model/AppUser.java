79f2516ef50b795f33afc75a515b9b335f643764
Wojciech Broniarczyk <wojciechbroniarczyk@gmail.com>
2025-04-21 20:14:53 +0200

    add user model

---
+++ hotel-backend/src/main/java/pl/wojtek/project/model/AppUser.java
@@ -0,0 +1,27 @@
+package pl.wojtek.project.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class AppUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String email;
+    private String password;
+    private String phone;
+}
