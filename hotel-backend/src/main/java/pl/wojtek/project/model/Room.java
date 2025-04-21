b049682343764589622a5785f4d65d1c7dc907b2
Wojciech Broniarczyk <wojciechbroniarczyk@gmail.com>
2025-04-21 20:17:37 +0200

    add room model

---
+++ hotel-backend/src/main/java/pl/wojtek/project/model/Room.java
@@ -0,0 +1,28 @@
+package pl.wojtek.project.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Room {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private int number;
+    private String type;
+    private int capacity;
+    private String status;
+    private double rate;
+}
