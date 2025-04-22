a2b2bf3147e5aca265005c8b74f87ab1e3bb523e
Wojciech Broniarczyk <wojciechbroniarczyk@gmail.com>
2025-04-22 18:45:17 +0200

    add endpoints for creating and retrieving rooms

---
+++ hotel-backend/src/main/java/pl/wojtek/project/controller/RoomController.java
@@ -0,0 +1,41 @@
+package pl.wojtek.project.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import pl.wojtek.project.model.Room;
+import pl.wojtek.project.service.RoomService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/rooms")
+public class RoomController {
+
+    private final RoomService roomService;
+
+    @Autowired
+    public RoomController(RoomService roomService) {
+        this.roomService = roomService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Room>> getAllRooms() {
+        List<Room> rooms = roomService.getAllRooms();
+        return new ResponseEntity<>(rooms, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Room> getRoomById(@PathVariable long id) {
+        Room room = roomService.getRoomById(id);
+        return new ResponseEntity<>(room, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Room> createRoom(@RequestBody Room room) {
+        Room createdRoom = roomService.createRoom(room);
+        return new ResponseEntity<>(createdRoom, HttpStatus.CREATED);
+    }
+}
