bb392f0fd2a5fc849950157700c3525999ad1f28
Wojciech Broniarczyk <wojciechbroniarczyk@gmail.com>
2025-05-05 00:49:05 +0200

    add navbar with routing

--- HotelManager-frontend/src/app/app-routing.module.ts
+++ HotelManager-frontend/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './component/about/about.component';
+import { HomeComponent } from './component/home/home.component';
+import { ContactComponent } from './component/contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: '**', redirectTo: '' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- HotelManager-frontend/src/app/app.module.ts
+++ HotelManager-frontend/src/app/app.module.ts
@@ -9,9 +9,20 @@ import { AppComponent } from './app.component';
 import { provideHttpClient } from '@angular/common/http';
 import { RoomCreateComponent } from './component/room-create/room-create.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { HomeComponent } from './component/home/home.component';
+import { AboutComponent } from './component/about/about.component';
+import { ContactComponent } from './component/contact/contact.component';
 
 @NgModule({
-  declarations: [AppComponent, RoomCreateComponent],
+  declarations: [
+    AppComponent,
+    RoomCreateComponent,
+    NavbarComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [provideClientHydration(), provideHttpClient()],
   bootstrap: [AppComponent],
