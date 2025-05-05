0b5dc4f1068a93cf5e2cd20c6c2585a50bed3d9a
Wojciech Broniarczyk <wojciechbroniarczyk@gmail.com>
2025-05-05 02:31:30 +0200

    add auth buttons to navbar

--- HotelManager-frontend/src/app/component/navbar/navbar.component.ts
+++ HotelManager-frontend/src/app/component/navbar/navbar.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
-
+import { Component, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { AuthService } from '../../service/auth.service';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
-
+  public isBrowser: boolean;
+  constructor(
+    @Inject(PLATFORM_ID) private platformId: any,
+    public auth: AuthService
+  ) {
+    this.isBrowser = isPlatformBrowser(this.platformId);
+  }
 }
