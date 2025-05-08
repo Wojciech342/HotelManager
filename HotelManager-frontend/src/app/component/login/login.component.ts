b2b3e0feecaa2d2670e46131a5cbf150b113e114
Wojciech Broniarczyk <wojciechbroniarczyk@gmail.com>
2025-05-08 18:43:14 +0200

    redesign login component

--- HotelManager-frontend/src/app/component/login/login.component.ts
+++ HotelManager-frontend/src/app/component/login/login.component.ts
@@ -1,41 +1,53 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { AuthService } from '../../service/auth.service';
-import { Router } from '@angular/router';
+import { Component, inject, OnInit } from '@angular/core';
+import { LoginInfo } from '../../auth/login-info';
+import { AuthService } from '../../auth/auth.service';
+import { TokenStorageService } from '../../auth/token-storage.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
 })
-export class LoginComponent {
-  loginForm: FormGroup;
+export class LoginComponent implements OnInit {
+  form: any = {};
+  isLoggedIn = false;
+  isLoginFailed = false;
+  errorMessage = '';
+  roles: string[] = [];
+  private loginInfo?: LoginInfo;
 
-  errorMsg: string | null = null;
+  private authService = inject(AuthService);
+  private tokenStorage = inject(TokenStorageService);
 
-  constructor(
-    private fb: FormBuilder,
-    private auth: AuthService,
-    private router: Router
-  ) {
-    this.loginForm = this.fb.group({
-      username: ['', Validators.required],
-      password: ['', Validators.required],
+  ngOnInit() {
+    if (this.tokenStorage.getToken() !== '{}') {
+      this.isLoggedIn = true;
+      this.roles = this.tokenStorage.getAuthorities();
+    }
+  }
+
+  onSubmit() {
+    this.loginInfo = new LoginInfo(this.form.username, this.form.password);
+
+    this.authService.attemptAuth(this.loginInfo).subscribe({
+      next: (data) => {
+        this.tokenStorage.saveToken(data.accessToken || '{}');
+        this.tokenStorage.saveUsername(data.username || '{}');
+        this.tokenStorage.saveAuthorities(data.authorities || []);
+
+        this.isLoginFailed = false;
+        this.isLoggedIn = true;
+        this.roles = this.tokenStorage.getAuthorities();
+        this.reloadPage();
+      },
+      error: (error) => {
+        this.errorMessage = error.error.message;
+        this.isLoginFailed = true;
+      },
     });
   }
 
-  onSubmit(): void {
-    if (this.loginForm.invalid) return;
-
-    const { username, password } = this.loginForm.value;
-    this.auth.login(username, password).subscribe({
-      next: () => {
-        this.router.navigateByUrl('/');
-        
-      },
-      error: (err) => {
-        this.errorMsg = err.error?.message || 'Login failed';
-      },
-    });
+  reloadPage() {
+    window.location.reload();
   }
 }
