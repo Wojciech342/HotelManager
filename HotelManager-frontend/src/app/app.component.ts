7a65d77e97fc7aeaa139f8b972382ce6a6277ccb
Wojciech Broniarczyk <wojciechbroniarczyk@gmail.com>
2025-05-08 18:47:40 +0200

    add user authority to token storage

--- HotelManager-frontend/src/app/app.component.ts
+++ HotelManager-frontend/src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import { TokenStorageService } from './auth/token-storage.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'HotelManager-frontend';
+
+  private roles?: string[];
+  authority?: string;
+
+  constructor(private tokenStorage: TokenStorageService) {}
+
+  ngOnInit() {
+    console.log('init');
+    if (this.tokenStorage.getToken()) {
+      console.log(this.tokenStorage.getToken());
+      this.roles = this.tokenStorage.getAuthorities();
+      this.roles.every((role) => {
+        if (role === 'ROLE_ADMIN') {
+          this.authority = 'admin';
+          return false;
+        }
+        this.authority = 'user';
+        return true;
+      });
+    }
+  }
 }
