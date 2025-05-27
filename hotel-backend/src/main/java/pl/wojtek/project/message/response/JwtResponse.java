0c6264a5a859d4c4b52a57e971797156759bba67
Wojciech Broniarczyk <wojciechbroniarczyk@gmail.com>
2025-05-27 11:59:04 +0200

    add lombok getters and setters

--- hotel-backend/src/main/java/pl/wojtek/project/message/response/JwtResponse.java
+++ hotel-backend/src/main/java/pl/wojtek/project/message/response/JwtResponse.java
@@ -1,8 +1,13 @@
 package pl.wojtek.project.message.response;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
 import java.util.Collection;
 
+
+@Getter
+@Setter
 public class JwtResponse {
 
     private String token;
@@ -14,33 +19,5 @@ public class JwtResponse {
         this.token = token;
         this.username = username;
         this.authorities = authorities;
-    }
-
-    public String getAccessToken() {
-        return token;
-    }
-
-    public void setAccessToken(String accessToken) {
-        this.token = accessToken;
-    }
-
-    public String getTokenType() {
-        return type;
-    }
-
-    public void setTokenType(String tokenType) {
-        this.type = tokenType;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return authorities;
     }
 }
--- hotel-backend/src/main/java/pl/wojtek/project/message/response/ResponseMessage.java
+++ hotel-backend/src/main/java/pl/wojtek/project/message/response/ResponseMessage.java
@@ -1,18 +1,15 @@
 package pl.wojtek.project.message.response;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class ResponseMessage {
 
     private String message;
 
     public ResponseMessage(String message) {
-        this.message = message;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
         this.message = message;
     }
 }
