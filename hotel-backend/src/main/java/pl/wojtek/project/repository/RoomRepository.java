0548858463cc26a94e6a5d3b047154c0a0c45b6e
Wojciech Broniarczyk <wojciechbroniarczyk@gmail.com>
2025-06-09 04:06:01 +0200

    remove not used findByNumber query

--- hotel-backend/src/main/java/pl/wojtek/project/repository/RoomRepository.java
+++ hotel-backend/src/main/java/pl/wojtek/project/repository/RoomRepository.java
@@ -4,8 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import pl.wojtek.project.model.Room;
 
-import java.util.Optional;
-
 public interface RoomRepository extends JpaRepository<Room, Long>, JpaSpecificationExecutor<Room> {
-    Optional<Room> findByNumber(int number);
 }
