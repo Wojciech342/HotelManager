f7126c57a7cc26658cc7e2f658bc5bd0b09b6f98
Wojciech Broniarczyk <wojciechbroniarczyk@gmail.com>
2025-06-09 04:10:06 +0200

    enhance page appearance

--- HotelManager-frontend/src/app/component/home/home.component.ts
+++ HotelManager-frontend/src/app/component/home/home.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AuthService } from '../../auth/auth.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  authService: AuthService = inject(AuthService);
+}
