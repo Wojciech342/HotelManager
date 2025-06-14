8ce49bbe0800c359c7182b3a293244e89e4c0566
Wojciech Broniarczyk <wojciechbroniarczyk@gmail.com>
2025-06-14 11:09:24 +0200

    handle reservations after room deletion

--- HotelManager-frontend/src/app/component/my-reservations/my-reservations.component.ts
+++ HotelManager-frontend/src/app/component/my-reservations/my-reservations.component.ts
@@ -84,6 +84,8 @@ export class MyReservationsComponent implements OnInit {
 
   canMakeReview(reservation: RoomReservation): boolean {
     if (reservation.review !== null) return false;
+    if (reservation.status === 'CANCELLED') return false;
+    if (reservation.room === null) return false;
     const today = new Date();
     const start = new Date(reservation.startDate + 'T00:00:00');
     return today >= start && !reservation.review;
