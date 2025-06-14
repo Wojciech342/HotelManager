39424a7239101865b0afd5123d67fd0bace6a96d
Wojciech Broniarczyk <wojciechbroniarczyk@gmail.com>
2025-06-14 11:03:55 +0200

    add checking for active or future reservations

--- hotel-backend/src/main/java/pl/wojtek/project/repository/RoomReservationRepository.java
+++ hotel-backend/src/main/java/pl/wojtek/project/repository/RoomReservationRepository.java
@@ -3,13 +3,17 @@ package pl.wojtek.project.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import pl.wojtek.project.model.RoomReservation;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
 public interface RoomReservationRepository extends JpaRepository<RoomReservation, Long>, PagingAndSortingRepository<RoomReservation, Long> {
     Optional<List<RoomReservation>> findByRoomId(Long roomId);
     Page<RoomReservation> findByUserId(Long userId, Pageable pageable);
+    @Query("SELECT COUNT(r) > 0 FROM RoomReservation r WHERE r.room.id = :roomId AND r.endDate >= :today")
+    boolean existsActiveOrFutureReservation(Long roomId, LocalDate today);
 }
