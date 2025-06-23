a9f54bb2c693f2ddfe17ec402a16b46832274e5d
Wojciech Broniarczyk <wojciechbroniarczyk@gmail.com>
2025-06-23 15:47:35 +0200

    add rating validation

--- hotel-backend/src/main/java/pl/wojtek/project/model/RoomReview.java
+++ hotel-backend/src/main/java/pl/wojtek/project/model/RoomReview.java
@@ -2,6 +2,9 @@ package pl.wojtek.project.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -25,5 +28,8 @@ public class RoomReview {
 
     private String description;
 
+    @NotNull
+    @Min(1)
+    @Max(5)
     private Double rating;
 }
