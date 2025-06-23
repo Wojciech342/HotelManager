051c58007d5dd10dd6422e156753c6ab9f1ae619
Wojciech Broniarczyk <wojciechbroniarczyk@gmail.com>
2025-06-23 15:42:45 +0200

    remove status from room adding sql query

--- hotel-backend/src/main/resources/rooms.sql
+++ hotel-backend/src/main/resources/rooms.sql
@@ -1,12 +1,12 @@
-INSERT INTO room (number, type, capacity, status, average_rating, price_per_night)
+INSERT INTO room (number, type, capacity, average_rating, price_per_night)
 VALUES
-(101, 'SINGLE', 1, 'AVAILABLE', 0.0, 100.00),
-(102, 'DOUBLE', 2, 'AVAILABLE', 0.0, 150.00),
-(103, 'FAMILY', 4, 'OCCUPIED', 0.0, 200.00),
-(104, 'SUITE', 2, 'MAINTENANCE', 0.0, 250.00),
-(105, 'DELUXE', 2, 'AVAILABLE', 0.0, 300.00),
-(106, 'SINGLE', 1, 'OCCUPIED', 0.0, 90.00),
-(107, 'DOUBLE', 2, 'AVAILABLE', 0.0, 160.00),
-(108, 'FAMILY', 4, 'AVAILABLE', 0.0, 210.00),
-(109, 'SUITE', 3, 'OCCUPIED', 0.0, 260.00),
-(110, 'DELUXE', 2, 'AVAILABLE', 0.0, 310.00);
+(101, 'SINGLE', 1, 0.0, 100.00),
+(102, 'DOUBLE', 2, 0.0, 150.00),
+(103, 'FAMILY', 4, 0.0, 200.00),
+(104, 'SUITE', 2, 0.0, 250.00),
+(105, 'DELUXE', 2, 0.0, 300.00),
+(106, 'SINGLE', 1, 0.0, 90.00),
+(107, 'DOUBLE', 2, 0.0, 160.00),
+(108, 'FAMILY', 4, 0.0, 210.00),
+(109, 'SUITE', 3, 0.0, 260.00),
+(110, 'DELUXE', 2, 0.0, 310.00);
